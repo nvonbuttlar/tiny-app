ac89d5e8c23c2e6e769e812e1870f3802b7de8f0
vagrant <nick.von.buttlar@gmail.com>
2018-10-03 03:40:17 +0000

    project initialization

---
+++ express_server.js
@@ -0,0 +1,29 @@
+
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+//first route added
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//second route added
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
