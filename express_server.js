68ecec8317cabe3460266ef0c64133206643695d
vagrant <nick.von.buttlar@gmail.com>
2018-10-03 18:40:40 +0000

    added views folder and 2 template files

--- express_server.js
+++ express_server.js
@@ -1,28 +1,41 @@
 
-var express = require("express");
+var express = require("express"); // this imports the express module
 var app = express();
 var PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // this tells the Express app to use EJS ad its templating engine
 
+
+
+// we use this database to keep track of all the URLs  and their shortened forms
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+// ROUTE METHODS USING GET
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//first route added
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//second route added
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => { // this passes our URL database to the urls_index template
+
+  let templateVars = {urls: urlDatabase}
+
+  res.render("urls_index", templateVars) //EJS knows to look to views folder for template files.ejs, therefor we can omit filename and pathway
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
