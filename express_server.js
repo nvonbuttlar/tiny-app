c5c950babbcb49207b8cbfb2b6ede83c8219d7da
vagrant <nick.von.buttlar@gmail.com>
2018-10-05 00:01:30 +0000

    login and logout, cookie features

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
 
-var express = require("express"); // this imports the express module
-var app = express();
-var PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
+const express      = require("express"); // this imports the express module
+const app          = express();
+const PORT         = 8080; // default port 8080
+const bodyParser   = require("body-parser");
+const cookieParser = require('cookie-parser')
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 app.set("view engine", "ejs"); // this tells the Express app to use EJS ad its templating engine
 
 // we use this database to keep track of all the URLs  and their shortened forms
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -16,7 +17,7 @@ var urlDatabase = {
 // Used function from StackOverflow
 let generateRandomString = function() {
   var randomURL = "";
-  var bank = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  var bank      = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
   for (var i = 0; i < 5; i++)
     randomURL += bank.charAt(Math.floor(Math.random() * bank.length));
@@ -27,22 +28,15 @@ let generateRandomString = function() {
 
 // ROUTE METHODS USING GET
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 // Renders page with list of urls
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase}
+  console.log(req.cookies);
+
+
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
   res.render("urls_index", templateVars); //EJS knows to look to views folder for template files.ejs, therefor we can omit filename and pathway
 });
 
@@ -58,13 +52,17 @@ app.post("/urls/:id/delete", (req, res) => {
 // Renders new URL page
 app.get("/urls/new", (req, res) => {
 
-  res.render("urls_new");
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
+
+  res.render("urls_new", templateVars);
 });
 
 // Redirects back to Homepage after submission
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  let longURL = req.body.longURL;
+  let longURL  = req.body.longURL;
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
 
@@ -75,7 +73,7 @@ app.post("/urls", (req, res) => {
 //Redirects page to the longURL webpagewhen short URL is passed to Search bar
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL  = urlDatabase[shortURL];
 
   res.redirect(longURL);
 });
@@ -85,22 +83,31 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies.username
   };
   res.render("urls_show", templateVars);
 });
 
 
 app.post("/urls/:id", (req, res) => {
-
   urlDatabase[req.params.id] = req.body.longURL;
 
   res.redirect("/urls");
 });
 
+//Post to login -- cookie will remain when navigating pages
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
 
+//Post to logout -- clear cookies and return to login (/urls)
+app.post("/logout", (req, res) => {
 
-
+  res.clearCookie("username", req.cookies.username);
+  res.redirect("/urls");
+})
 
 
 
